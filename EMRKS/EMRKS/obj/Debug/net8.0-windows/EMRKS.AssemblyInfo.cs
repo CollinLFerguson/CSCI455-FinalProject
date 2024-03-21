78b2343d4339c4b68d7d51bd1e390a7f17461bb1
johnguido <connorwaggoner98@gmail.com>
2024-03-21 09:06:02 -0400

    test push

--- EMRKS/EMRKS/obj/Debug/net8.0-windows/EMRKS.AssemblyInfo.cs
+++ EMRKS/EMRKS/obj/Debug/net8.0-windows/EMRKS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMRKS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5d856dff106e13b55085234c90b83bf8d266738")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff5616aad41a53b49df5a819b6209940bf9313a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMRKS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMRKS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

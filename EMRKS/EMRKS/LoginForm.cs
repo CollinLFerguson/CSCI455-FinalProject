625a4ebe82f035d0ac3951c5a9ca8bb0c3a29337
Collin Ferguson <collinleeferguson@gmail.com>
2024-03-21 15:58:24 -0400

    Update LoginForm.cs

--- EMRKS/EMRKS/LoginForm.cs
+++ EMRKS/EMRKS/LoginForm.cs
@@ -21,18 +21,13 @@ namespace EMRKS
         {
             if (this.MdiParent == null) { this.Close(); return;} //Prevents the rare case where a login form was created without a parent.
 
-
-
             //validateUser(txtUsername.text, txtPin.text);
                       
-            
             //IF AUTHORIZED DO THE EXECUTE THE BELOW CODE
             
             this.Hide();
             ((MainForm)this.MdiParent).loadLanding();
             this.Close();
-
-
         }
     }
 }

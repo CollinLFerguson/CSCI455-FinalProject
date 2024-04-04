4de1f5505120c924f21fa5777d41ac113ed556c8
johnguido <connorwaggoner98@gmail.com>
2024-04-04 15:50:58 -0400

    init Patient.cs

---
+++ EMRKS/EMRKS/Patient.cs
@@ -0,0 +1,34 @@
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMRKS
+{
+    internal class Patient
+    {
+        private string SSN;
+        private DateOnly DOB;
+        private int age;
+        private char sex;
+        private string firstName;
+        private char middleInit;
+        private string lastName;
+        private string primaryDoctorID;
+        private string phoneNumber;
+
+        public Patient(string SSN, DateOnly DOB, int age, char sex, string firstName, char middleInit, string lastName, string primaryDoctorID, string phoneNumber) { 
+            this.SSN = SSN;
+            this.DOB = DOB;
+            this.age = age;
+            this.sex = sex;
+            this.firstName = firstName;
+            this.middleInit = middleInit;
+            this.lastName = lastName;
+            this.primaryDoctorID = primaryDoctorID;
+            this.phoneNumber = phoneNumber;
+        }
+    }
+}

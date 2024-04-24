10c1f5aeea9caf60430c4728b9777783269136c0
Daisy <Daisy.dpr@gmail.com>
2024-04-24 15:18:17 -0500

    updated login screen to include logo

--- EMRKS/EMRKS/LoginForm.Designer.cs
+++ EMRKS/EMRKS/LoginForm.Designer.cs
@@ -28,30 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             label1 = new Label();
             button1 = new Button();
             txtUsername = new TextBox();
             label2 = new Label();
             txtPin = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
-            label1.Location = new Point(141, 127);
+            label1.Location = new Point(936, 246);
             label1.Name = "label1";
-            label1.Size = new Size(164, 30);
+            label1.Size = new Size(143, 25);
             label1.TabIndex = 0;
             label1.Text = "Member Login";
             // 
             // button1
             // 
             button1.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            button1.Location = new Point(141, 332);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(950, 405);
             button1.Name = "button1";
-            button1.Size = new Size(121, 41);
+            button1.Size = new Size(106, 32);
             button1.TabIndex = 1;
             button1.Text = "Sign In";
             button1.UseVisualStyleBackColor = true;
@@ -60,51 +62,60 @@
             // txtUsername
             // 
             txtUsername.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            txtUsername.Location = new Point(141, 194);
-            txtUsername.Margin = new Padding(3, 4, 3, 4);
+            txtUsername.Location = new Point(936, 297);
             txtUsername.Multiline = true;
             txtUsername.Name = "txtUsername";
             txtUsername.PlaceholderText = "ID Number";
-            txtUsername.Size = new Size(163, 31);
+            txtUsername.Size = new Size(143, 25);
             txtUsername.TabIndex = 2;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(141, 402);
+            label2.Location = new Point(950, 462);
             label2.Name = "label2";
-            label2.Size = new Size(123, 19);
+            label2.Size = new Size(106, 15);
             label2.TabIndex = 4;
             label2.Text = "Trouble signing in?";
             // 
             // txtPin
             // 
             txtPin.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            txtPin.Location = new Point(141, 261);
-            txtPin.Margin = new Padding(3, 4, 3, 4);
+            txtPin.Location = new Point(936, 351);
             txtPin.Name = "txtPin";
             txtPin.PasswordChar = '*';
             txtPin.PlaceholderText = "Pin";
-            txtPin.Size = new Size(163, 28);
+            txtPin.Size = new Size(143, 25);
             txtPin.TabIndex = 5;
             txtPin.UseSystemPasswordChar = true;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-1, -2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(720, 720);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.DarkSeaGreen;
-            ClientSize = new Size(430, 561);
+            BackColor = Color.FromArgb(106, 167, 119);
+            ClientSize = new Size(1280, 720);
+            Controls.Add(pictureBox1);
             Controls.Add(txtPin);
             Controls.Add(label2);
             Controls.Add(txtUsername);
             Controls.Add(button1);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(3, 4, 3, 4);
             Name = "LoginForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -116,5 +127,6 @@
         private TextBox txtUsername;
         private Label label2;
         private TextBox txtPin;
+        private PictureBox pictureBox1;
     }
 }

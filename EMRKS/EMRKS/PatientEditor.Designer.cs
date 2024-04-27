ab4ee78e9d034e904a846d57b5967cc485f2819e
Daisy <Daisy.dpr@gmail.com>
2024-04-27 18:18:12 -0500

    color fix

--- EMRKS/EMRKS/PatientEditor.Designer.cs
+++ EMRKS/EMRKS/PatientEditor.Designer.cs
@@ -318,7 +318,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.DarkSeaGreen;
+            BackColor = Color.SeaGreen;
             Controls.Add(button1);
             Controls.Add(textBox13);
             Controls.Add(label15);
